fa59790e64fcfc38bb3b7c5b301f31b82399a0ee
Oladipo Olasemo <dipolash4@gmail.com>
2022-06-03 14:05:07 +0100

    initial commit

---
+++ challenge/mix.exs
@@ -0,0 +1,28 @@
+defmodule Challenge.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :challenge,
+      version: "0.1.0",
+      elixir: "~> 1.13",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:faker, "~> 0.16", only: :test},
+      {:credo, "~> 1.6.4", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
